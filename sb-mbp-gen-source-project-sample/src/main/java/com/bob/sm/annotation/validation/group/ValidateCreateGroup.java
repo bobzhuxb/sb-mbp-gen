3473e82ccd74333285ee7b33310963c5c2085e3f
zhuxibo <zhuxibo_8@126.com>
2019-03-23 09:21:29 +0800

    validation验证BUG修复

--- sb-mbp-gen-source-project-sample/src/main/java/com/bob/sm/annotation/validation/group/ValidateCreateGroup.java
+++ sb-mbp-gen-source-project-sample/src/main/java/com/bob/sm/annotation/validation/group/ValidateCreateGroup.java
@@ -1,7 +1,9 @@
 package com.bob.sm.annotation.validation.group;
 
+import javax.validation.groups.Default;
+
 /**
  * 生成器生成的create方法的验证组
  */
-public interface ValidateCreateGroup {
+public interface ValidateCreateGroup extends Default {
 }
--- sb-mbp-gen-source-project-sample/src/main/java/com/bob/sm/annotation/validation/group/ValidateUpdateGroup.java
+++ sb-mbp-gen-source-project-sample/src/main/java/com/bob/sm/annotation/validation/group/ValidateUpdateGroup.java
@@ -1,7 +1,9 @@
 package com.bob.sm.annotation.validation.group;
 
+import javax.validation.groups.Default;
+
 /**
  * 生成器生成的update方法的验证组
  */
-public interface ValidateUpdateGroup {
+public interface ValidateUpdateGroup extends Default {
 }
