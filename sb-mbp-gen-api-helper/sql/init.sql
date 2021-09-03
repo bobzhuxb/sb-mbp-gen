9ef44c9d33e9b17287dd2bdbaa9271ae85fce5c7
bob <zhuxibo_8@126.com>
2021-09-03 10:36:34 +0800

    更新

--- sb-mbp-gen-api-helper/sql/init.sql
+++ sb-mbp-gen-api-helper/sql/init.sql
@@ -1,82 +1,85 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : api_helper
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80021
+ Source Host           : localhost:3306
+ Source Schema         : api_helper
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2021-08-31 22:37:36
+ Target Server Type    : MySQL
+ Target Server Version : 80021
+ File Encoding         : 65001
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for ah_class_code
 -- ----------------------------
 DROP TABLE IF EXISTS `ah_class_code`;
-CREATE TABLE `ah_class_code` (
-  `id` char(50) NOT NULL COMMENT '主键',
-  `package_name` varchar(100) DEFAULT NULL COMMENT '包名',
-  `class_name` varchar(50) DEFAULT NULL COMMENT '类名',
-  `insert_time` varchar(20) DEFAULT NULL COMMENT '插入时间',
-  `update_time` varchar(20) DEFAULT NULL COMMENT '更新时间',
-  `ah_project_id` char(50) DEFAULT NULL COMMENT '项目ID',
+CREATE TABLE `ah_class_code`  (
+  `id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `package_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '包名',
+  `class_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类名',
+  `insert_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '插入时间',
+  `update_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新时间',
+  `ah_project_id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '项目ID',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_project` (`ah_project_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='可用实体类';
+  INDEX `idx_project`(`ah_project_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '可用实体类' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ah_field
 -- ----------------------------
 DROP TABLE IF EXISTS `ah_field`;
-CREATE TABLE `ah_field` (
-  `id` char(50) NOT NULL COMMENT '主键ID',
-  `type_name` varchar(100) DEFAULT NULL COMMENT '字段类型名',
-  `generic_type_name` varchar(100) DEFAULT NULL COMMENT '泛型名',
-  `field_name` varchar(50) DEFAULT NULL COMMENT '字段名',
-  `insert_time` varchar(20) DEFAULT NULL COMMENT '插入时间',
-  `update_time` varchar(20) DEFAULT NULL COMMENT '更新时间',
-  `ah_class_code_id` char(50) DEFAULT NULL COMMENT '实体类ID',
+CREATE TABLE `ah_field`  (
+  `id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键ID',
+  `type_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '字段类型名',
+  `generic_type_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '泛型名',
+  `field_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '字段名',
+  `descr` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '注释',
+  `insert_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '插入时间',
+  `update_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新时间',
+  `ah_class_code_id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '实体类ID',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_class_code` (`ah_class_code_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  INDEX `idx_class_code`(`ah_class_code_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ah_interface
 -- ----------------------------
 DROP TABLE IF EXISTS `ah_interface`;
-CREATE TABLE `ah_interface` (
-  `id` char(50) NOT NULL COMMENT '主键',
-  `inter_no` varchar(50) DEFAULT NULL COMMENT '接口号',
-  `http_url` varchar(200) DEFAULT NULL COMMENT 'URL地址',
-  `http_method` varchar(10) DEFAULT NULL COMMENT 'Http方法',
-  `add_default_prefix` varchar(10) DEFAULT NULL COMMENT '是否有默认前缀',
-  `inter_descr` varchar(200) DEFAULT NULL COMMENT '描述',
-  `return_type` varchar(100) DEFAULT NULL COMMENT '返回类型全名',
-  `data_json` text COMMENT '配置JSON',
-  `insert_time` varchar(20) DEFAULT NULL COMMENT '插入时间',
-  `update_time` varchar(20) DEFAULT NULL COMMENT '更新时间',
-  `ah_project_id` char(50) DEFAULT NULL COMMENT '项目ID',
+CREATE TABLE `ah_interface`  (
+  `id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `inter_no` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '接口号',
+  `http_url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'URL地址',
+  `http_method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'Http方法',
+  `add_default_prefix` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '是否有默认前缀',
+  `inter_descr` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '描述',
+  `return_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '返回类型全名',
+  `data_json` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '配置JSON',
+  `insert_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '插入时间',
+  `update_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新时间',
+  `ah_project_id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '项目ID',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_project` (`ah_project_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='接口';
+  INDEX `idx_project`(`ah_project_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '接口' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ah_project
 -- ----------------------------
 DROP TABLE IF EXISTS `ah_project`;
-CREATE TABLE `ah_project` (
-  `id` char(50) NOT NULL COMMENT '主键',
-  `name` varchar(50) DEFAULT NULL COMMENT '名称',
-  `descr` varchar(50) DEFAULT NULL COMMENT '描述',
-  `url_prefix` varchar(50) DEFAULT NULL COMMENT 'URL前缀',
-  `base_package` varchar(50) DEFAULT NULL COMMENT '基础包名',
-  `insert_time` varchar(20) DEFAULT NULL COMMENT '插入时间',
-  `update_time` varchar(20) DEFAULT NULL COMMENT '更新时间',
+CREATE TABLE `ah_project`  (
+  `id` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
+  `descr` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '描述',
+  `url_prefix` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'URL前缀',
+  `base_package` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '基础包名',
+  `insert_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '插入时间',
+  `update_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='项目';
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '项目' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
